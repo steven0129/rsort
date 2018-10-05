400a33ebd19270f7a1977bdbad356a7ed7d7e9e9
steven0129 <h3584935@gmail.com>
2018-10-05 22:53:13 +0800

    Clean garbage

--- main.c
+++ main.c
@@ -21,62 +21,50 @@ int main(int argc, char *argv[]) {
     }
 
     FILE* inFile;
-    if(argc > 1) {
+    if(argc > 1)
         inFile = fopen(argv[argc - 1], "r");
-    } else {
+    else
         inFile = stdin;
-    }
 
     if(inFile == NULL) {
         printf("No such file or could not open it.\n");
         exit(1);
     }
     
-    int debug = 0;
     int counter = 0;
+    char hex[5];
     ROW rows[400000];
     wchar_t delimeter[] = L"\n";
     wchar_t* keyPat = L"@T";
     wchar_t* end = L"@GAISRec:";
     wchar_t line[BUFFER_SIZE];
+    wchar_t* buffer;
 
     while(fgetws(line, sizeof(line), inFile) != NULL) {
-        wchar_t* tmp = (wchar_t*)malloc(sizeof(wchar_t) * ((int)wcslen(line) + 1));
-        wchar_t* str;
-        wcscpy(tmp, line);
-        str = wcscat_m(str, tmp);
+        buffer = wcscat_m(buffer, line);
         rows[counter].idx = counter;
 
         while(fgetws(line, sizeof(line), inFile) != NULL) {
-            size_t newSize = sizeof(wchar_t) * ((int)wcslen(line) + 1);
-            tmp = (wchar_t*)malloc(newSize);
-            wcscpy(tmp, line);
-
-            if(wcsstr(tmp, end)) {
-                rows[counter].data = (wchar_t*)malloc(sizeof(wchar_t) * ((int)wcslen(str) + 1));
-                wcscpy(rows[counter].data, str);
+            if(wcsstr(line, end)) {
+                rows[counter].data = (wchar_t*)malloc(sizeof(wchar_t) * ((int)wcslen(buffer) + 1));
+                wcscpy(rows[counter].data, buffer);
                 counter++;
-                wcscpy(str, L"");
+                wcscpy(buffer, L"");
                 break;
             }
 
-            str = wcscat_m(str, tmp);
+            buffer = wcscat_m(buffer, line);
         }
-
-        free(tmp);
     }
 
     for(int i=0; i < counter; i++) {
-        wchar_t* buffer;
-        char *ptr;
-        char hex[5];
         wchar_t* data = (wchar_t*)malloc(sizeof(wchar_t) * ((int)wcslen(rows[i].data) + 1));
         wcscpy(data, rows[i].data);
-        wchar_t* result = wcsstr(data, keyPat);
-        result = wcstok(result, L"\n", &buffer);
-        result = wcstok(result, L":", &buffer);
-        result = wcstok(NULL, L":", &buffer);
-        sprintf(hex, "%04x", result[0]);
+        data = wcsstr(data, keyPat);
+        data = wcstok(data, L"\n", &buffer);
+        data = wcstok(data, L":", &buffer);
+        data = wcstok(NULL, L":", &buffer);
+        sprintf(hex, "%04x", data[0]);
         rows[i].key = hex2dec(hex);
     }
 
