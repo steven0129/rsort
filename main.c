1de80ba0c36f23dac2c991c092ee40b16e804789
steven0129 <h3584935@gmail.com>
2018-11-17 22:44:11 +0800

    Move file pointer

--- main.c
+++ main.c
@@ -13,6 +13,8 @@
 #define TOLOWER(Ch) towlower (Ch)
 #define CHUNK_SIZE 1024
 
+long int fileLen(FILE*);
+
 int main(int argc, char* argv[]) {
     if (!setlocale(LC_CTYPE, "")) {
         fprintf(stderr, "Error:Please check LANG, LC_CTYPE, LC_ALL.\n");
@@ -22,11 +24,10 @@ int main(int argc, char* argv[]) {
     FILE* inFile;
     FILE* outFile;
     int fileNum = 0;
-    char* chunk = (char*)malloc(sizeof(char) * CHUNK_SIZE);
+    
     char* BEGIN = "@\n@url";
     char* KEY_PAT = "@content:";
-    ROW* rows = (ROW*)malloc(sizeof(char) * (CHUNK_SIZE + 10) + sizeof(int) * (CHUNK_SIZE + 10) * 2);
-
+    
     if(argc > 1)
         inFile = fopen(argv[argc - 1], "r");
     else
@@ -37,25 +38,42 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
-    fseek(inFile, 0, SEEK_SET);
-    fread(chunk, sizeof(char), CHUNK_SIZE, inFile);
-    
-    for(int counter = 0;; counter++) {
-        char hex[9];
-        int dec;
-        rows[counter].data = strdup(chunk);
-        if(strstr(rows[counter].data + strlen(BEGIN), BEGIN) == NULL) {
-            counter--;
-            break;
+    for(long int fp = 0; fp < fileLen(inFile); fileNum++) {
+        printf("%d\n", fileNum);
+        char* chunk = (char*)malloc(sizeof(char) * CHUNK_SIZE);
+        ROW* rows = (ROW*)malloc(sizeof(char) * (CHUNK_SIZE + 10) + sizeof(int) * (CHUNK_SIZE + 10) * 2);
+        fseek(inFile, fp, SEEK_SET);
+        fread(chunk, sizeof(char), CHUNK_SIZE, inFile);
+        
+        for(int counter = 0;; counter++) {
+            char hex[9];
+            int dec;
+            rows[counter].data = strdup(chunk);
+            
+            if(strstr(rows[counter].data + strlen(BEGIN), BEGIN) == NULL) {
+                counter--;
+                break;
+            }
+
+            *strstr(rows[counter].data + strlen(BEGIN), BEGIN) = '\0';
+            fp += strlen(rows[counter].data);
+            sprintf(hex, "%08x", *(strstr(rows[counter].data, KEY_PAT) + strlen(KEY_PAT)));
+            dec = hex2dec(hex);
+            rows[counter].key = dec;
+            rows[counter].fileNum = fileNum;
+            chunk = strdup(strstr(chunk + strlen(BEGIN), BEGIN));
         }
-        *strstr(rows[counter].data + strlen(BEGIN), BEGIN) = '\0';
-        sprintf(hex, "%08x", *(strstr(rows[counter].data, KEY_PAT) + strlen(KEY_PAT)));
-        dec = hex2dec(hex);
-        rows[counter].key = dec;
-        rows[counter].fileNum = fileNum;
-        chunk = strdup(strstr(chunk + strlen(BEGIN), BEGIN));
+
+        free(chunk);
+        free(rows);
     }
+    
 
     fclose(inFile);
     return 0;
+}
+
+long int fileLen(FILE* file) {
+    fseek(file, 0, SEEK_END);
+    return ftell(file);
 }
